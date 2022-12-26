c83457c7111e02240df196e952fbb8d021221b83
Cristian Diaz <cristian.d5299@gmail.com>
2022-12-25 19:47:18 -0500

    using java docs!

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        int salario = 1000;
-        salario+=200;
-        salario-=50;
-        System.out.println(salario);
+        Main mainClass = new Main();
+        String nombre = mainClass.returnName("Cristian");
+        System.out.println(mainClass.separateName(nombre));
+    }
+
+    /**
+     * DESCRIPCIÓN: Devuelve un string que se le pase.
+     * @param string es una cadena de texto cualquiera.
+     * @return devuelve la cadena de texto.
+     * */
+    public String returnName(String string){
+        return string;
+    }
+
+    /**
+     * DESCRIPCIÓN: Esta función recibe un String y separa cada letra con espacios.
+     *
+     * @param string una cadena de texto a separar por espacios.
+     * @return final name Devuelve el nombre separado por espacios.
+     * */
+    public String separateName(String string){
+
+        StringBuilder nameWithSpaces = new StringBuilder();
+        char[] separedName = string.toCharArray();
+        String finalName = "";
+
+        for (char letra : separedName){
+            nameWithSpaces.append(letra).append(" ");
+        }
+
+        int finalChar = nameWithSpaces.length() - 1;
+        finalName = String.valueOf(nameWithSpaces.deleteCharAt(finalChar));
+        return finalName;
     }
 }
